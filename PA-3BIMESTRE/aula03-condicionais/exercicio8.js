e50bfe27dbb91d22a50b45016c88ba292a3bd1d4
th1ag018 <thiago.fitipaldi1224@gmail.com>
2024-08-12 19:05:34 -0300

    Update exercicio8.js

--- PA-3BIMESTRE/aula03-condicionais/exercicio8.js
+++ PA-3BIMESTRE/aula03-condicionais/exercicio8.js
@@ -3,7 +3,7 @@ let horasextras = 8
 let horastrabalhadas = (horas + horasextras)
 
 if (horastrabalhadas>40) {
-    console.log(`O funcionário trabalhou ${horastrabalhadas} horas extras"`)
+    console.log(`O funcionário atingiu sua jornada de trabalho de 40 horas e adcionou mais ${horasextras} como horas extras"`)
 } else {
-    console.log(`O funcionário trabalhou sua jornada de 40 horas`)
+    console.log(`O funcionário não atingiu sua jornada de trabalho de 40 horas`)
 }
